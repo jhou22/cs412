ff6acee6d871bfee1c7fc7d1f197d98cbe8122d6
jhou22@bu.edu <jhou22@bu.edu>
2024-09-23 18:30:29 -0400

    fix css

--- cs412/urls.py
+++ cs412/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hw/", include("hw.urls")), ## new path
-    path("quotes/", include("quotes.urls"))
+    path("/", include("quotes.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
